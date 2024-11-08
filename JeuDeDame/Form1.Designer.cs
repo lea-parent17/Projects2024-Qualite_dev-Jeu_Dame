8c20d894bfd64ab242ddbf08200d03ea8057de43
Nathan <nathan.bugeaud60@gmail.com>
2024-11-08 19:02:50 +0100

    Jeu

---
+++ JeuDeDame/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Forms;
+
+namespace JeuDeDames
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button btnDamesFrancaises;
+        private Button btnDamesEuropeennes;
+
+        // Méthode pour libérer les ressources
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Méthode d'initialisation des composants
+        private void InitializeComponent()
+        {
+            this.btnDamesFrancaises = new System.Windows.Forms.Button();
+            this.btnDamesEuropeennes = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+            // 
+            // btnDamesFrancaises
+            // 
+            this.btnDamesFrancaises.Location = new System.Drawing.Point(100, 100);
+            this.btnDamesFrancaises.Name = "btnDamesFrancaises";
+            this.btnDamesFrancaises.Size = new System.Drawing.Size(280, 60);
+            this.btnDamesFrancaises.TabIndex = 0;
+            this.btnDamesFrancaises.Text = "Dames Françaises (8x8)";
+            this.btnDamesFrancaises.UseVisualStyleBackColor = true;
+            this.btnDamesFrancaises.Click += new System.EventHandler(this.btnDamesFrancaises_Click);
+            // 
+            // btnDamesEuropeennes
+            // 
+            this.btnDamesEuropeennes.Location = new System.Drawing.Point(100, 200);
+            this.btnDamesEuropeennes.Name = "btnDamesEuropeennes";
+            this.btnDamesEuropeennes.Size = new System.Drawing.Size(280, 60);
+            this.btnDamesEuropeennes.TabIndex = 1;
+            this.btnDamesEuropeennes.Text = "Dames Européennes (10x10)";
+            this.btnDamesEuropeennes.UseVisualStyleBackColor = true;
+            this.btnDamesEuropeennes.Click += new System.EventHandler(this.btnDamesEuropeennes_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(480, 480);
+            this.Controls.Add(this.btnDamesFrancaises);
+            this.Controls.Add(this.btnDamesEuropeennes);
+            this.Name = "Form1";
+            this.Text = "Jeu de Dames";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ JeuDeDame/FormJeu.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace JeuDeDames
+{
+    public partial class FormJeu : Form
+    {
+        private int boardSize;  // La taille du plateau (8 ou 10)
+
+        public FormJeu(int size)
+        {
+            InitializeComponent();
+            boardSize = size;
+            InitializeBoard(boardSize);
+        }
+
+        private void InitializeBoard(int size)
+        {
+            this.Controls.Clear();  // Efface les contrôles existants
+
+            // Créer les boutons pour chaque case du plateau
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button btn = new Button();
+                    btn.Size = new System.Drawing.Size(60, 60);
+                    btn.Location = new System.Drawing.Point(j * 60, i * 60);
+                    btn.TabIndex = i * size + j;
+                    btn.Click += new EventHandler(this.Case_Click);
+                    this.Controls.Add(btn);
+                }
+            }
+
+            this.ClientSize = new System.Drawing.Size(size * 60, size * 60);  // Ajuster la taille du formulaire
+        }
+
+        private void Case_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            MessageBox.Show($"Vous avez cliqué sur la case {btn.TabIndex + 1}");
+        }
+
+        // Ajoutez la méthode pour gérer l'événement de clic sur le bouton "Dames Françaises"
+        private void btnDamesFrancaises_Click(object sender, EventArgs e)
+        {
+            // Appel de la fonction pour initialiser le plateau en 8x8
+            FormJeu formJeu = new FormJeu(8);  // Plateau 8x8
+            formJeu.Show();  // Affiche le formulaire de jeu
+        }
+    }
+}
