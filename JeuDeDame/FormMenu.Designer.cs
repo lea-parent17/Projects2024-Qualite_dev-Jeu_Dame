20a584847649db1d3c0bf11c782648d2bbb81aa7
lea-parent17 <lea.parent17@gmail.com>
2024-12-10 12:59:59 +0100

    Amélioration qualité du code
    Suppression de fichier inutile
    Ajout de commentaire correct

--- JeuDeDame/FormMenu.Designer.cs
+++ JeuDeDame/FormMenu.Designer.cs
@@ -1,9 +1,14 @@
-﻿namespace JeuDeDames
+﻿using System.Windows.Forms;
+
+namespace JeuDeDames
 {
     partial class FormMenu
     {
         private System.ComponentModel.IContainer components = null;
+        private Button btnDamesFrancaises;
+        private Button btnDamesInternationales;
 
+        // Méthode pour libérer les ressources
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -13,37 +18,42 @@
             base.Dispose(disposing);
         }
 
+        // Méthode d'initialisation des composants
         private void InitializeComponent()
         {
             this.btnDamesFrancaises = new System.Windows.Forms.Button();
-            this.btnDamesEuropeennes = new System.Windows.Forms.Button();
+            this.btnDamesInternationales = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
             // btnDamesFrancaises
-            this.btnDamesFrancaises.Location = new System.Drawing.Point(50, 30);
+            // 
+            this.btnDamesFrancaises.Location = new System.Drawing.Point(33, 39);
             this.btnDamesFrancaises.Name = "btnDamesFrancaises";
-            this.btnDamesFrancaises.Size = new System.Drawing.Size(200, 50);
-            this.btnDamesFrancaises.Text = "Dames Françaises";
+            this.btnDamesFrancaises.Size = new System.Drawing.Size(280, 60);
+            this.btnDamesFrancaises.TabIndex = 0;
+            this.btnDamesFrancaises.Text = "Dames Françaises (8x8)";
             this.btnDamesFrancaises.UseVisualStyleBackColor = true;
             this.btnDamesFrancaises.Click += new System.EventHandler(this.btnDamesFrancaises_Click);
-
+            // 
             // btnDamesEuropeennes
-            this.btnDamesEuropeennes.Location = new System.Drawing.Point(50, 100);
-            this.btnDamesEuropeennes.Name = "btnDamesEuropeennes";
-            this.btnDamesEuropeennes.Size = new System.Drawing.Size(200, 50);
-            this.btnDamesEuropeennes.Text = "Dames Européennes";
-            this.btnDamesEuropeennes.UseVisualStyleBackColor = true;
-            this.btnDamesEuropeennes.Click += new System.EventHandler(this.btnDamesEuropeennes_Click);
-
+            // 
+            this.btnDamesInternationales.Location = new System.Drawing.Point(33, 136);
+            this.btnDamesInternationales.Name = "btnDamesInternationales";
+            this.btnDamesInternationales.Size = new System.Drawing.Size(280, 60);
+            this.btnDamesInternationales.TabIndex = 1;
+            this.btnDamesInternationales.Text = "Dames Internationales (10x10)";
+            this.btnDamesInternationales.UseVisualStyleBackColor = true;
+            this.btnDamesInternationales.Click += new System.EventHandler(this.btnDamesInternationales_Click);
+            // 
             // FormMenu
-            this.ClientSize = new System.Drawing.Size(300, 200);
+            // 
+            this.ClientSize = new System.Drawing.Size(344, 232);
             this.Controls.Add(this.btnDamesFrancaises);
-            this.Controls.Add(this.btnDamesEuropeennes);
-            this.Text = "Menu - Jeu de Dames";
+            this.Controls.Add(this.btnDamesInternationales);
+            this.Name = "FormMenu";
+            this.Text = "Jeu de Dames";
             this.ResumeLayout(false);
-        }
 
-        private System.Windows.Forms.Button btnDamesFrancaises;
-        private System.Windows.Forms.Button btnDamesEuropeennes;
+        }
     }
 }
--- JeuDeDame/FormMenu.cs
+++ JeuDeDame/FormMenu.cs
@@ -3,23 +3,44 @@ using System.Windows.Forms;
 
 namespace JeuDeDames
 {
+    /// <summary>
+    /// Classe représentant le menu principal du jeu de dames
+    /// Permet de choisir entre les modes de jeu "Dames Françaises" et "Dames Européennes"
+    /// </summary>
     public partial class FormMenu : Form
     {
+        /// <summary>
+        /// Initialise une nouvelle instance du formulaire de menu principal
+        /// </summary>
         public FormMenu()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Gestionnaire d'événement pour le bouton "Dames Françaises (8x8)"
+        /// Ouvre un nouveau formulaire de jeu avec un plateau 8x8 et masque le menu principal
+        /// </summary>
+        /// <param name="sender">Source de l'événement</param>
+        /// <param name="e">Arguments de l'événement</param>
         private void btnDamesFrancaises_Click(object sender, EventArgs e)
         {
             FormJeu formJeu = new FormJeu(8); // Plateau 8x8
             formJeu.Show();
+            this.Hide();
         }
 
-        private void btnDamesEuropeennes_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Gestionnaire d'événement pour le bouton "Dames Européennes (10x10)"
+        /// Ouvre un nouveau formulaire de jeu avec un plateau 10x10 et masque le menu principal
+        /// </summary>
+        /// <param name="sender">Source de l'événement</param>
+        /// <param name="e">Arguments de l'événement</param>
+        private void btnDamesInternationales_Click(object sender, EventArgs e)
         {
             FormJeu formJeu = new FormJeu(10); // Plateau 10x10
             formJeu.Show();
+            this.Hide();
         }
     }
 }
